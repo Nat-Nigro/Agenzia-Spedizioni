a043f02a74f939fef12a6979e0c00da93989a8ad
Nat-Nigro <natalenigro15@gmail.com>
2024-03-07 15:15:27 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,176 @@
+﻿using AziendaSpedizioni.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AziendaSpedizioni.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(Utenti u)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDb"].ConnectionString.ToString();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM Utenti WHERE Username = @Username AND Password = @Password";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Username", u.Username);
+                    cmd.Parameters.AddWithValue("@Password", u.Password);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        FormsAuthentication.SetAuthCookie(u.Username, false);
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Username o password errati";
+                        return View();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message;
+                    return View();
+                }
+                
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CheckCodFiscale(string CodFisc)
+        {
+            string pattern = @"^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(CodFisc))
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult CheckPIVA(string PIVA)
+        {
+            {
+                string pattern = @"^\d{11}$";
+                Regex regex = new Regex(pattern);
+                if (regex.IsMatch(PIVA))
+                {
+                    return Json(true, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(false, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+        DateTime miaData = DateTime.Today;
+        public ActionResult checkDataSpedizione(DateTime data)
+            {
+            DateTime oggi = DateTime.Today;
+            miaData = data;
+            if (data >= oggi)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult checkDataConsegna(DateTime data)
+        {
+            if (data > miaData)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Tracking()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Tracking(string CodiceFiscale, string codTracciamento)
+        {
+            ViewBag.search = codTracciamento;
+            string connectionString = ConfigurationManager.ConnectionStrings["connectionStringDb"].ConnectionString.ToString();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM Spedizioni s JOIN Clienti c ON s.IdCliente = c.idCliente WHERE (c.CodiceFiscale = @CodiceFiscale OR c.PartitaIva = @CodiceFiscale) AND s.codTracciamento = @codTracciamento";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@CodiceFiscale", CodiceFiscale);
+                    cmd.Parameters.AddWithValue("@codTracciamento", codTracciamento);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        Spedizioni s = new Spedizioni();
+                        s.IdSpedizione = Convert.ToInt32(dr["IdSpedizione"]);
+                        s.codTracciamento = dr["codTracciamento"].ToString();
+                        s.dataSpedizione = Convert.ToDateTime(dr["dataSpedizione"]);
+                        s.pesoSpedizione = Convert.ToDecimal(dr["pesoSpedizione"]);
+                        s.cittaDestinazione = dr["cittaDestinazione"].ToString();
+                        s.indirizzoDestinazione = dr["indirizzoDestinazione"].ToString();
+                        s.nominativoDestinatario = dr["nominativoDestinatario"].ToString();
+                        s.costoSpedizione = Convert.ToDecimal(dr["costoSpedizione"]);
+                        s.dataConsegna = Convert.ToDateTime(dr["dataConsegna"]);
+                        return View(s);
+                    }
+                    else
+                    {
+                        TempData["Message"] = "Nessuna spedizione trovata";
+                        return RedirectToAction("Index");
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = ex.Message;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return View();
+        }
+    }
+}
